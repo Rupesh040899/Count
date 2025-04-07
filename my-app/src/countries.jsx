5a6960b21b0bc6f5a137283719be7091a74b807b
Rupesh <rupesh.mali0408@gmail.com>
2025-04-07 10:23:23 +0530

    n

--- my-app/src/countries.jsx
+++ my-app/src/countries.jsx
@@ -4,15 +4,15 @@ export default function CitySelector() {
   const [count, setCount] = useState(0);
 
   const handleChange = useCallback(() => {
-    if (count > 0) {
+    
       setCount(count - 1);
-    }
+    
   });
 
   return (
     <div>
       <h1>Counter App</h1>
-      <h2>{count}</h2>
+      <h2>Count: {count}</h2>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={handleChange}>Decrement</button>
     </div>
